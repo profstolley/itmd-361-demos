8dfeaa8219fea8cecaf6d0c79b7be89bc4b3f4ac
Karl Stolley <karl.stolley@gmail.com>
2016-10-19 20:00:01 -0500

    Mostly nonsense but functional JavaScript from class

---
+++ 10-19/js/site.js
@@ -0,0 +1,19 @@
+var html = '<a href="about/">About Karl’s Class</a>';
+
+var username = 'Rick';
+var favorite_colors = ['red','blue','yellow','green'];
+
+function greetUser(username,colors) {
+  console.log("Hello, " + username + "!");
+  console.log("You like " + colors.length + " colors");
+}
+
+var user = {
+  name: 'Rick',
+  favorite_colors: ['red','blue','yellow','green']
+};
+
+var dog = { name: 'Hank', cuteness: '100' };
+
+
+
