eca1b9851e4be934d90968626969acacaaad6a3c
Karl Stolley <karl.stolley@gmail.com>
2016-10-24 16:30:36 -0500

    Completed beer song functions

--- 10-24/js/site.js
+++ 10-24/js/site.js
@@ -1,1 +1,40 @@
+// bottleSong(99,'Coke');
+function bottleSong(num,beverage) {
+  // var num, beverage
+  var onum = num;
+  while(num >= 0) {
+    console.log(bottlesOnTheWall(num,beverage));
+    console.log(bottlesOnTheWall(num,beverage,''));
+    console.log(takeOneDown(num));
+    num--;
+    if (num === -1) {
+      console.log(bottlesOnTheWall(onum,beverage));
+    }
+    else {
+      console.log(bottlesOnTheWall(num,beverage));
+    }
+    console.log('');
+  }
+}
+
+function bottlesOnTheWall(num,beverage,place=' on the wall',onum) {
+  var s = 's';
+  if (num === 1) {
+    var s = '';
+  }
+  if (num === 0) {
+    num = 'No more'
+  }
+  return num + ' bottle' + s + ' of ' + beverage + place;
+}
+
+function takeOneDown(num) {
+  if (num === 0) {
+    return 'You go to the store and buy some more';
+  }
+  else {
+    return 'You take one down and pass it around';
+  }
+}
+
 
