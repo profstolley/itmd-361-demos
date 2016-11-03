4d442cb915258c230e1dccae0fea82005d55d3ef
Karl Stolley <karl.stolley@gmail.com>
2016-11-03 09:54:27 -0500

    Self-invoking function to load jQuery & protect global namespace

--- 11-02/js/site.js
+++ 11-02/js/site.js
@@ -1,1 +1,12 @@
-
+// Tell jQuery to give up the dollar sign
+$.noConflict();
+// Instead, use a self-invoking function to have a private
+// dollar sign
+(function($){
+  $(document).ready(
+    function() {
+      $('html').removeClass('nojs');
+      $('html').addClass('hasjs');
+    }
+  );
+})(jQuery);
