ed3e1eac7eb29961999ef7b0c976f91acea6bd4a
Karl Stolley <karl.stolley@gmail.com>
2016-11-07 16:58:32 -0600

    Added jQuery to handle text-sizing events; moved textSize() function into self-closing func

--- 11-07/js/site.js
+++ 11-07/js/site.js
@@ -7,30 +7,44 @@ $.noConflict();
     function() {
       $('html').removeClass('nojs');
       $('html').addClass('hasjs');
+      $('#header').append('<ul id="textsizer">\n<li><a id="smaller" href="#null">Smaller</a></li>\n<li><a id="bigger" href="#null">Bigger</a></li>\n</ul>');
+
+      $('#smaller').on('click', function(e) {
+        $('html').removeClass(textSize(0)).addClass(textSize(-1));
+        e.preventDefault();
+      });
+      $('#bigger').on('click', function(e) {
+        $('html').removeClass(textSize(0)).addClass(textSize(1));
+        e.preventDefault();
+      });
+
+// Example for opening external links in new windows.
+// NEVER DO THIS.
+    /*
+      $('a[href^=http]').on('click', function(e) {
+        window.open(this.href);
+        e.preventDefault();
+      });
+    */
+
     }
   );
 
-  // Need to add/remove text-sizing classes
-  // Need also to add HTML elements to handle
-  // click events to fire the textSize()
-  // function, which should probably be here
-  // in the ready event, too.
+  // Function to adjust text sizes
+  var i;
+  function textSize(n) {
+    var sizes = ['smallest', 'smaller', 'normal', 'larger', 'largest'];
+    if (typeof(i) === 'undefined') {
+      i = 2; // start with 'normal'
+    }
+    i = i + n; // increase or decrease
+    if (i >= sizes.length) {
+      i = sizes.length - 1; // stay within the array's values
+    } else if (i < 0) {
+      i = 0; // stay within the array's values
+    }
+    console.log(i);
+    return sizes[i];
+  }
 
 })(jQuery);
-
-// Function to adjust text sizes
-var i;
-function textSize(n) {
-  var sizes = ['smallest', 'smaller', 'normal', 'larger', 'largest'];
-  if (typeof(i) === 'undefined') {
-    i = 2; // start with 'normal'
-  }
-  i = i + n; // increase or decrease
-  if (i >= sizes.length) {
-    i = sizes.length - 1; // stay within the array's values
-  } else if (i < 0) {
-    i = 0; // stay within the array's values
-  }
-  console.log(i);
-  return sizes[i];
-}
