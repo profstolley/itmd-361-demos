b2dcc2cf5d16cb8a01415f614764362cd38764c0
Karl Stolley <karl.stolley@gmail.com>
2016-11-14 18:17:47 -0600

    JavaScript from class demo to query for a GitHub user's login and html_url

--- 11-14/js/site.js
+++ 11-14/js/site.js
@@ -1,3 +1,25 @@
 // TODO: Use a function closure and release global $
 $(document).ready(function() {
+  /*
+  $('#primary').append('<p>I am first</p>');
+  $.get(
+    'https://api.github.com/users/karlstolley',
+    function(data) {
+      $('#primary').append(
+        '<p><a href="'+ data.html_url+'">'+data.login+'</a></p>'
+      );
+    });
+  $('#primary').append('<p>I am third</p>');
+  */
+  $('#gh-form').on('submit', function(event) {
+    var query = $('#gh-username').val();
+    $.get(
+      'https://api.github.com/users/' + query,
+      function(data) {
+        $('#primary').append(
+          '<p><a href="'+ data.html_url+'">'+data.login+'</a></p>'
+        );
+      });
+    event.preventDefault();
+  });
 });
